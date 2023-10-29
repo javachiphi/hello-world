5414bd5e6fb6c1109f86338244bcd44ca8190001
Sophie Y <javachiphi@gmail.com>
2023-10-29 15:31:28 +0800

    Add global styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 
 import './App.css';
-import MainBanner from './components/MainBanner/MainBanner';
+import PhilosophySection from './views/PhilosophySection/PhilosophySection';
+import HeroSection from './views/HeroSection/HeroSection';
+import BenefitSection from './views/BenefitSection/BenefitSection';
+import ProductSection from './views/ProductSection/ProductSection';
+import CallToActionSection from './views/CallToActionSection/CallToActionSection';
+import Footer from './views/Footer/Footer'
 
 function App() {
   return (
     <div className="App">
-      <MainBanner />
+      <HeroSection />
+      <PhilosophySection />
+      <BenefitSection />
+      <ProductSection />
+      <CallToActionSection />
+      <Footer />
     </div>
   );
 }
